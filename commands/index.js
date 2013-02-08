8f065f45ce47f67668384394c072c9c9f16802cd
Julian Duque <julianduquej@gmail.com>
2013-02-07 20:49:50 -0500

    [feature] named endpointsstill a WIP needs to be improved

--- commands/index.js
+++ commands/index.js
@@ -3,3 +3,4 @@ var commands = exports;
 commands.design = require('./design');
 commands.document = require('./document');
 commands.database = require('./database');
+commands.endpoint = require('./endpoint');
