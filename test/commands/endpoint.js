5a6181f2a5073360e46f2b89c7092ea31207492e
Julian Duque <julianduquej@gmail.com>
2013-12-17 20:28:15 -0500

    [tests] Add default endpoint test

--- test/commands/endpoint.js
+++ test/commands/endpoint.js
@@ -78,6 +78,23 @@ specify('futon endpoint add myendpoint http://user:pass@mycouch.com/db', functio
 
 });
 
+specify('futon endpoint add test http://localhost:5984/test', function (assert) {
+  assert.expect(6);
+
+  run('futon endpoint add test http://localhost:5984/test')
+  .expect(function (response) {
+    futon_ok(assert, response);
+
+    var conf = get_config();
+    assert.ok(conf['endpoints']);
+    assert.ok(conf['endpoints']['test']);
+    assert.equal(conf['endpoints']['test'], 'http://localhost:5984/test');
+  })
+  .end(function () {
+    assert.ok(true);
+  });
+
+});
 
 
 specify('futon endpoint set', function (assert) {
@@ -112,6 +129,24 @@ specify('futon endpoint set myendpoint', function (assert) {
 
 });
 
+specify('futon endpoint set test', function (assert) {
+  assert.expect(5);
+
+  run('futon endpoint set test')
+  .expect(function (response) {
+    futon_ok(assert, response);
+
+    var conf = get_config();
+
+    assert.ok(conf['endpoint']);
+    assert.equal(conf['endpoint'], 'http://localhost:5984/test');
+  })
+  .end(function () {
+    assert.ok(true);
+  });
+
+});
+
 specify('futon endpoint delete', function (assert) {
   assert.expect(3);
 
@@ -133,6 +168,22 @@ specify('futon endpoint delete myendpoint', function (assert) {
 
     var conf = get_config();
     assert.equal(typeof conf['endpoints']['myendpoint'], 'undefined');
+  })
+  .end(function () {
+    assert.ok(true);
+  });
+
+});
+
+specify('futon endpoint delete test', function (assert) {
+  assert.expect(4);
+
+  run('futon endpoint delete test')
+  .expect(function (response) {
+    futon_ok(assert, response);
+
+    var conf = get_config();
+    assert.equal(typeof conf['endpoints']['test'], 'undefined');
   })
   .end(function () {
     assert.ok(true);
